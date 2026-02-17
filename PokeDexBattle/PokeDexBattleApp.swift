a0363b7b56344db558213c2bf4142297501597f3
cesarnr <sergio.cesar.12@gmail.com>
2026-02-17 17:46:20 -0600

    Initial Commit

---
+++ PokeDexBattle/PokeDexBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexBattleApp.swift
+//  PokeDexBattle
+//
+//  Created by Sergio Cesar Nieto Ramirez on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
